5858f9362c657ab81d745013de468c14f873c08d
PascalUlor <pascalulor@yahoo.com>
2019-06-13 12:04:35 +0100

    add css loader

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,77 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import styled, { keyframes } from 'styled-components';
 import Home from './component/Home';
 
 const baseUrl = 'http://localhost:5000/friends';
+
+/**
+ * Styles
+ */
+
+const Bars = keyframes`
+    0% {
+    top: 6px;
+    height: 51px;
+  }
+  50%, 100% {
+    top: 19px;
+    height: 6rem;
+  }`;
+
+const Loader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  position: relative;
+  width: 6rem;
+  height: 64px;
+  margin: 10rem auto;
+div {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  /* position: absolute; */
+  margin: 10rem auto;
+  left: 6px;
+  width: 1rem;
+  background: #000;
+  animation: ${Bars} 1.2s cubic-bezier(0, 0.5, 0.5, 1) infinite;
+}
+div:nth-child(1) {
+  left: 6px;
+  animation-delay: -0.24s;
+}
+div:nth-child(2) {
+  left: 26px;
+  animation-delay: -0.12s;
+}
+div:nth-child(3) {
+  left: 45px;
+  animation-delay: 0;
+}
+`;
 class App extends Component{
   constructor() {
     super()
     this.state = {
-      friends: ''
+      friends: '',
+      errorMessage: '',
+      loader: false,
     }
   }
 
   FetchFriends() {
+    this.setState({loader: true})
     axios.get(`${baseUrl}`)
     .then(res =>{
       console.log("======",res);
       this.setState({
         friends: res.data
       })
-    }).catch(err=> console.log("---------",err));
+    })
+    .catch(err=> console.log("---------",err))
+    .finally(()=> this.setState({loader: false}));
   }
 
   AddFriends(dataInput) {
@@ -43,11 +96,13 @@ class App extends Component{
   render() {
     return (
       <div>
-        {/* {this.state.friends !== '' ? <Home friendDb = {this.state.friends} />:
-        null}  */}
         {this.state.friends && <Home friendDb = {this.state.friends}
         AddFriend={this.AddFriends}
         />}
+        {
+          this.state.spinner &&
+          <Loader><div></div><div></div><div></div></Loader>
+        }
       </div>
     );
   }
